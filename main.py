3a5d2a6336b0f81cd3b40c3e2bdf2ca5fc85c359
killua <132150277+lilputtt@users.noreply.github.com>
2023-05-20 18:22:23 +0300

    Update main.py
    v.2

--- main.py
+++ main.py
@@ -30,21 +30,51 @@ class Player(GameSprite):
             self.rect.y += 5
 
 clock = time.Clock()
-game = True
 
-rocket1 = Player('бита.png', 0, 50, 50, 20, 150)
-rocket2 = Player('бита.png', 0, 630, 50, 20, 150)
+game = True
+finish = False
+
+rocket1 = Player('палка.png', 0, 50, 50, 20, 150)
+rocket2 = Player('палка.png', 0, 630, 50, 20, 150)
+ball = GameSprite('bald.png', 0, 250, 250, 50, 50)
 
 window = display.set_mode((700, 500))
-background = transform.scale(image.load('бейп.jpg'), (700, 500))
+background = transform.scale(image.load('небо.jpeg'), (700, 500))
+
+font.init()
+font1 = font.Font(None, 40)
+lose1 = font1.render('PLAYER 1 LOSE!', True, (180 , 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180 , 0, 0))
+
+speed_y = 3
+speed_x = 3
 
 while game:
-    window.blit(background, (0,0))
+    if finish != True:
 
-    rocket1.reset()
-    rocket1.update_l()
-    rocket2.reset()
-    rocket2.update_r()
+        window.blit(background, (0,0))
+
+        rocket1.reset()
+        rocket1.update_l()
+        rocket2.reset()
+        rocket2.update_r()
+        ball.reset()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if ball.rect.y > 500 - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(rocket1, ball) or sprite.collide_rect(rocket2, ball):
+            speed_x *= -1
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+
+    if ball.rect.x > 650:
+        finish = True
+        window.blit(lose2, (200, 200))
 
     for e in event.get():
         if e.type == QUIT:
